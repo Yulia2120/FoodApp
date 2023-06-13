66a26d6a3a8258cb214a4fcade7c626d6a69a295
Yuliya <iksulia1@gmail.com>
2023-06-13 19:41:05 +0300

    add FoodListAdapter

--- app/src/main/java/com/obushko/foodapp/Activity/MainActivity.java
+++ app/src/main/java/com/obushko/foodapp/Activity/MainActivity.java
@@ -1,16 +1,45 @@
 package com.obushko.foodapp.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.obushko.foodapp.Adapter.FoodListAdapter;
+import com.obushko.foodapp.Domain.FoodDomain;
 import com.obushko.foodapp.R;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FoodListAdapter foodListAdapter;
+    private RecyclerView recyclerViewFood;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+        ArrayList<FoodDomain> items = new ArrayList<>();
+        items.add(new FoodDomain("Cheese Burger", "Satisfy your cravings with our juicy Cheese Burger. \n"+
+                "Made with 100% Angus beef patty and topped with\n"+
+                "melted cheddar cheese, fresh lettuce, tomato, and\n"+
+                "our secret sauce, this classic burger will leave you\n"+
+                "wanting more. Served with crispy fries and a drink,\n"+
+                "it's the perfect meal for any occasion.", "fast_1", 15, 20, 120, 4));
+        items.add(new FoodDomain("Pizza Peperoni", "Get a taste of Italy with our delicious Pepperoni Pizza. Made with freshly rolled dough,zesty tomato sauce, mozzarella cheese and topped with spicy pepperoni slices, this pizza is sure to be a crowd-pleaser.", "fast_2", 10, 25, 200, 5));
+        items.add(new FoodDomain("Vegetable Pizza", "Looking for a healthier option? Iry our Vegetable Pizza, made with a variety of fresh veggies such as bell peppers, onions, mushrooms, olives, and tomatoes. Topped with mozzarella cheese and a tangy tomato sauce, this pizza is full of flavor and goodness.", "fast_3", 13, 30, 100, 4.5));
+
+        recyclerViewFood = findViewById(R.id.recyclerView1);
+        recyclerViewFood.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+        foodListAdapter = new FoodListAdapter(items);
+        recyclerViewFood.setAdapter(foodListAdapter);
     }
 }
--- app/src/main/java/com/obushko/foodapp/Adapter/FoodListAdapter.java
+++ app/src/main/java/com/obushko/foodapp/Adapter/FoodListAdapter.java
@@ -1,4 +1,69 @@
 package com.obushko.foodapp.Adapter;
 
-public class FoodListAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.GranularRoundedCorners;
+import com.obushko.foodapp.Domain.FoodDomain;
+import com.obushko.foodapp.R;
+
+import java.util.ArrayList;
+
+public class FoodListAdapter extends RecyclerView.Adapter<FoodListAdapter.ViewHolder> {
+
+    ArrayList<FoodDomain> items;
+    Context context;
+
+    public FoodListAdapter(ArrayList<FoodDomain> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_food_list, parent, false);
+        context = parent.getContext();
+        return new ViewHolder(inflate);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.titleTxt.setText(items.get(position).getTitle());
+        holder.timeTxt.setText(items.get(position).getTime() + "min");
+        holder.scoreTxt.setText("" +items.get(position).getScore());
+        int drawableResourceId = holder.itemView.getResources().getIdentifier(items.get(position).getPicUrl(),
+                "drawable", holder.itemView.getContext().getPackageName());
+
+        Glide.with(holder.itemView.getContext())
+                .load(drawableResourceId)
+                .transform(new GranularRoundedCorners(30,30,0, 0))
+                .into(holder.pic);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView titleTxt, timeTxt, scoreTxt;
+        ImageView pic;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titleTxt = itemView.findViewById(R.id.titleTxt);
+            timeTxt = itemView.findViewById(R.id.timeTxt);
+            scoreTxt = itemView.findViewById(R.id.scoreTxt);
+            pic = itemView.findViewById(R.id.pic);
+        }
+    }
 }
